6aa492b061eb77c85f79db96fa3758ac7a8ecde5
IDM <mary.ryan@lit.ie>
2022-10-21 14:55:33 +0100

    Create Quotes.js

---
+++ Quotes.js
@@ -0,0 +1,22 @@
+[
+  {
+    text: "Aithníonn ciaróg, ciaróg eile.",
+    author: "test"
+  },
+  {
+    text: "Ní dhéanfadh an saol capall rása d’asal.",
+    author: "test"
+  },
+   {
+    text: "Fillean an feall ar an bhfeallaire.",
+    author: "test"
+  },
+   {
+    text: "Tús maith leath na hoibre.",
+    author: "test"
+  },
+   {
+    text: "Níl saoi gan locht.",
+    author: "test"
+  }
+]
